fc2a1f848f22ddd1b64d2200e91f159f2e23ce58
Khalid Faisal <37410601+Khalid-Faisal@users.noreply.github.com>
2021-08-29 17:37:55 +0530

    Add files via upload

--- myProject1.sql
+++ myProject1.sql
@@ -1,27 +1,25 @@
-create table user1(user_id number primary key,name varchar2(150),phone_no number,email varchar2(100),address varchar2(100),reg_date date,password varchar2(100),upload_photo long);
-create sequence userid start with 100 increment by 1;
+create database amdocs_project;
+use amdocs_project;
 
-insert into user1(user_id,name,phone_no,email,address,reg_date,password,upload_photo) values(userid.nextval,'Ankit',4578008234,'ankit@gmail.com','noida','03-jan-2021','ankit','img.jpg');
+create table user1(user_id int(5) primary key auto_increment,name varchar(250),phone_no long,email varchar(120),address varchar(255),reg_date date,password varchar(100),upload_photo long)auto_increment=100;
+insert into user1(user_id,name,phone_no,email,address,reg_date,password,upload_photo) values(null,'Ankit',4578008234,'ankit@gmail.com','noida','03-jan-2021','ankit','img.jpg');
+select * from user1;
+-- drop table user1;
 
-select user_id from user1;
-create table contact(user_id number references user1 , name varchar2(150), Email varchar2(50), Phone_no number, Messege varchar2(200), contact_id number primary key);
-create sequence contactid start with 1 increment by 1;
+create table contact(user_id int(5) references user1 , name varchar(250), email varchar(120), Phone_no long, messege varchar(255), contact_id int(5) primary key auto_increment);
+insert into contact values (101, 'Ankit', 'ankit@gmail.com',4578008234 , 'Message',null);
+select * from contact;
+-- update contact set user_id=102 where contact_id=2;
 
-insert into contact values (121, 'Ankit', 'ankit@gmail.com',4578008234 , 'Message',contactid.nextval);
-select contact_id from contact;
-
-create table admin (admin_id number primary key, name varchar2(100),email varchar2(100), password varchar2(100));
-create sequence adminid start with 1 increment by 1;
-
-insert into admin values (adminid.nextval,'ankit','ankit@gmail.com','pass');
+-- drop table contact;
+create table admin (admin_id int(5) primary key auto_increment, name varchar(250),email varchar(120) not null, password varchar(100) not null)auto_increment=100;
+insert into admin values (null,'ankit','ankit@gmail.com','pass');
 select admin_id from admin;
 
-create table course (course_id number primary key, c_name varchar2(150),c_desp varchar2(200), c_fees number,c_resource varchar2(200));
-create sequence courseid start with 110 increment by 1;
-insert into course values (courseid.nextval,'JAVA','Programing language',299, 'video.mp4');
-select course_id from course;
+create table course (course_id int(5) primary key auto_increment, c_name varchar(250),c_desp varchar(250), c_fees double(8,2) default 999.00,c_resource varchar(250) default 'NA')auto_increment=1000;
+insert into course values (null,'JAVA','Programing_language',299, 'video.mp4');
+select * from course;
 
-create table feedback(user_id number references user1,name varchar2(150),email varchar2(100),f_id number primary key,feedback varchar2(200));
-create sequence fbid start with 0 increment by 1;
-
-insert into feedback(user_id,name,email,f_id,feedback)values(121,'ankit','ankit@gmail.com',fbid.nextval,'good');
+create table feedback(user_id int(5) references user1,name varchar(250),email varchar(120),f_id int(5) primary key auto_increment,feedback varchar(250));
+insert into feedback(user_id,name,email,f_id,feedback)values(101,'ankit','ankit@gmail.com',null,'good');
+select * from feedback;
